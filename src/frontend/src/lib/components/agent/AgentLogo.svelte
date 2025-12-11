878012de961ed394d07fecf6aefee2b02cc472a3
Rahees <ahdrahees.dev@gmail.com>
2025-12-11 19:16:20 +0530

    feat: click blink effect on agent logo

--- src/frontend/src/lib/components/agent/AgentLogo.svelte
+++ src/frontend/src/lib/components/agent/AgentLogo.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	const withBg = '/agent/logo/agent-logo-with-bg.png';
 	const withoutBg = '/agent/logo/agent-logo-no-bg.png';
 
 	let isWithBg = true;
 	let klass = 'size-12';
 	export { klass as class };
+	let clickBlinkTimeout: NodeJS.Timeout;
 
 	onMount(() => {
 		let blinkTimout: NodeJS.Timeout;
@@ -34,10 +35,22 @@
 			clearTimeout(blinkTimout3);
 		};
 	});
+
+	onDestroy(() => {
+		clearTimeout(clickBlinkTimeout);
+	});
 </script>
 
 <img
 	class="{klass} mix-blend-lighten transition-opacity"
 	src={isWithBg ? withBg : withoutBg}
 	alt="Agent Logo 1"
+	onclick={() => {
+		clearTimeout(clickBlinkTimeout);
+
+		isWithBg = !isWithBg;
+		clickBlinkTimeout = setTimeout(() => {
+			isWithBg = !isWithBg;
+		}, 100);
+	}}
 />
