7f2043166b07c57535cfaa97aeb09887f83390cd
Rahees <ahdrahees.dev@gmail.com>
2025-12-13 18:33:12 +0530

    updated script to use local folder as artifact service

--- scripts/run-agents.sh
+++ scripts/run-agents.sh
@@ -1,2 +1,7 @@
+#!/bin/bash 
 cd agents
-poetry run adk web --allow_origins http://localhost:5173 --session_service_uri sqlite://./.local-state/agent_session_service_db.sqlite3 --logo-text "Gefifi Agents" --logo-image-url "https://gefifi.at0.app/icons/icon-144x144.png"
+poetry run adk web --allow_origins http://localhost:5173 \
+  --session_service_uri sqlite://./.local-state/agent_session_service_db.sqlite3 \
+  --artifact_service_uri="file://$PWD/.local-state/artifacts" \
+  --logo-text "Gefifi Agents" \
+  --logo-image-url "https://gefifi.at0.app/icons/icon-144x144.png"
